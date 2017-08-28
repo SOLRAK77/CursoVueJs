cafa99144b9e8418d4cf81e80df694098272f737
carlos-linux <ccastillo.perez77@gmail.com>
2017-08-28 14:07:34 -0500

    Leccion 11 use de la API

--- platzimusic/src/api/config.js
+++ platzimusic/src/api/config.js
@@ -2,7 +2,7 @@ export default {
     appname : 'platzimusic',
     apiKEY : '590e5b4fc077ea76e2068292b0f5d99f',
     secret : 'e3ecb9cb7b5d00bd348818b59e21915c',
-    registeredTo : SOLRAK77
+    registeredTo : 'SOLRAK77'
 }
 /*
 Application name 	platzimusic
---
+++ platzimusic/src/api/index.js
@@ -0,0 +1,13 @@
+import config from './config'
+
+//const apiKey = config.apiKey
+//La linea de abajo equivale a lo mismo de la linea de arriba
+//(obtener el valor apiKey del Archivo config)
+const {apiKEY}  = config
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=mexico&api_key=${apiKEY}&format=json`
+
+export default function getArtists(){
+    return fetch (URL)
+        .then(res => res.json())
+        .then(json => json.topartists.artist)
+}
