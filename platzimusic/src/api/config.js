3e133b531f075a11652a77f5f88d2a70ef05f362
carlos-linux <ccastillo.perez77@gmail.com>
2017-08-28 12:39:26 -0500

    Leccion 10_1, cambion los elementos base

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appname : 'platzimusic',
+    apiKEY : '590e5b4fc077ea76e2068292b0f5d99f',
+    secret : 'e3ecb9cb7b5d00bd348818b59e21915c',
+    registeredTo : SOLRAK77
+}
+/*
+Application name 	platzimusic
+API key 	590e5b4fc077ea76e2068292b0f5d99f
+Shared secret 	e3ecb9cb7b5d00bd348818b59e21915c
+Registered to 	SOLRAK77
+*/
